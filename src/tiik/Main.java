6ba03b29783668198e95fe03d69d83fd2a5ed7c4
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2018-01-10 12:40:01 +0100

    Branch specificly for lz78 so it won't interfere with development of other things such as the class 'SomeStatistics'.

--- src/tiik/Main.java
+++ src/tiik/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tiik;
 
 import java.io.ByteArrayInputStream;
@@ -23,22 +18,12 @@ import java.util.Map;
 import tiik.lz78.LZ78;
 import tiik.lz78.LZ78Exception;
 
-/**
- *
- * @author Marcin
- */
 public class Main {
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
-		
 		if (args.length == 1) {
 			final File file = new File(args[0]);
 			try (final FileInputStream inputStream = new FileInputStream(file)) {
-				//testStatistics(inputStream);
 				testCompression(inputStream);
 			} catch (FileNotFoundException e) {
 				System.err.println("No such file!");
@@ -48,57 +33,8 @@ public class Main {
 				return;
 			}
 		} else {
-			//testStatistics(System.in);
 			testCompression(System.in);
 		}
-	}
-	
-	private static void testStatistics(final InputStream inputStream) {
-		//byte[] bytes = "Example text".getBytes(Charset.forName("windows-1252"));
-		//SomeStatistics ss = new SomeStatistics(bytes);
-		
-		SomeStatistics ss;
-		try {
-			ss = new SomeStatistics(inputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-
-		final Map<Byte, Double> empiricalProbabilities = ss.getEmpiricalProbabilities();
-		final Map<Byte, Double> quantitiesOfInformation = ss.getQuantitiesOfInformation();
-		System.out.println("char: probability qantity-of-information");
-		List<Byte> keys = new LinkedList<>(empiricalProbabilities.keySet());
-	Comparator<Byte> unsignedByteComparator = new Comparator<Byte>() {
-			@Override
-			public int compare(final Byte byte1, final Byte byte2) {
-				final int int1 = byteToUnsignedInt(byte1);
-				final int int2 = byteToUnsignedInt(byte2);
-				if (int1 < int2)
-					return -1;
-				else if (int1 > int2)
-					return 1;
-				else
-					return 0;
-			}
-			private int byteToUnsignedInt(byte b) {
-				return b >= 0 ? b : b + 256;
-			}
-		};
-		Collections.sort(keys, unsignedByteComparator);
-		for (byte b : keys) {
-			System.out.print(String.format("0x%02X ", b));
-			try {
-				System.out.print(String.format("'%c'", b));
-			} catch (IllegalFormatCodePointException e) {
-				System.out.print("   ");
-			}
-			System.out.println(String.format(": %2.4f%% %1.4f", empiricalProbabilities.get(b) * 100, quantitiesOfInformation.get(b)));
-		}
-
-		System.out.println();
-
-		System.out.println("Binary entropy: " + ss.getBinaryEntropy());
 	}
 	
 	private static void testCompression(InputStream inputStream) {
