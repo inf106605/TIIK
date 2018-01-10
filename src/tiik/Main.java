add3d25130e5d0bc6630c59df7cf282e814bfdfc
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2018-01-10 13:11:41 +0100

    Convert test program to usable compressor, working in command line.

--- src/tiik/Main.java
+++ src/tiik/Main.java
@@ -1,30 +1,57 @@
 package tiik;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.IllegalFormatCodePointException;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.lang.NumberFormatException;
+
 import tiik.lz78.LZ78;
 import tiik.lz78.LZ78Exception;
 
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			final File file = new File(args[0]);
+		int offset = 0;
+		
+		boolean compress = true;
+		int dictLimit = 0;
+		while (args.length > offset && args[offset].charAt(0) == '-') {
+			switch(args[offset]) {
+				case "-d":
+					compress = false;
+					++offset;
+					break;
+				case "-s":
+					++offset;
+					if (args.length == offset) {
+						System.err.println("Option '-s' needs an argument!");
+						return;
+					}
+					try {
+						dictLimit = Integer.parseInt(args[offset]);
+					} catch (NumberFormatException e) {
+						System.err.println("'" + args[offset] + "' is not a number!");
+						return;
+					}
+					if (dictLimit < 0) {
+						System.err.println("Dictionary size limit cannot be less than 0!");
+						return;
+					}
+					++offset;
+					break;
+				default:
+					System.err.println("Unknown option '" + args[offset] + "'!");
+					return;
+			}
+		}
+		
+		if (args.length - offset == 1) {
+			final File file = new File(args[offset]);
 			try (final FileInputStream inputStream = new FileInputStream(file)) {
-				testCompression(inputStream);
+				doTheWork(inputStream, System.out, dictLimit, compress);
 			} catch (FileNotFoundException e) {
 				System.err.println("No such file!");
 				return;
@@ -33,67 +60,19 @@ public class Main {
 				return;
 			}
 		} else {
-			testCompression(System.in);
+			doTheWork(System.in, System.out, dictLimit, compress);
 		}
 	}
 	
-	private static void testCompression(InputStream inputStream) {
-		byte[] originalBytes, compressedBytes, decompressedBytes;
-		{ // read data
-			int readedBytes;
-			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-			byte[] data = new byte[16384];
-			try {
-				while ((readedBytes = inputStream.read(data, 0, data.length)) != -1)
-					buffer.write(data, 0, readedBytes);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-			originalBytes = buffer.toByteArray();
-		}
-		{ // compress
-			inputStream = new ByteArrayInputStream(originalBytes);
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			LZ78 lz78 = new LZ78(originalBytes.length / 8);
-			try {
+	private static void doTheWork(final InputStream inputStream, final OutputStream outputStream, final int dictLimit, final boolean compress) {
+		LZ78 lz78 = new LZ78(dictLimit);
+		try {
+			if (compress)
 				lz78.compress(inputStream, outputStream);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-			compressedBytes = outputStream.toByteArray();
-			System.err.println("Compresion:\n" + lz78);
-			//lz78.printDebugInfo();
-		}
-		System.err.println();
-		{ // decompress
-			inputStream = new ByteArrayInputStream(compressedBytes);
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			LZ78 lz78 = new LZ78();
-			try {
-				lz78.decompress(inputStream, outputStream);
-			} catch (IOException | LZ78Exception e) {
-				e.printStackTrace();
-				return;
-			}
-			decompressedBytes = outputStream.toByteArray();
-			System.err.println("Decompresion:\n" + lz78);
-			//lz78.printDebugInfo();
-		}
-		System.err.println();
-		{ // estimate Huffman
-			SomeStatistics ss = new SomeStatistics(originalBytes);
-			System.err.println("Estimated huffman size of plain data:\t\t" + ((int)(originalBytes.length * ss.getBinaryEntropy() / 8.0)));
-			ss = new SomeStatistics(compressedBytes);
-			System.err.println("Estimated huffman size of compressed data:\t" + ((int)(compressedBytes.length * ss.getBinaryEntropy() / 8.0)));
-		}
-		System.err.println();
-		{ // compare
-			if (Arrays.equals(originalBytes, decompressedBytes))
-				System.err.println("Success!");
 			else
-				System.err.println("Fuck!");
+				lz78.decompress(inputStream, outputStream);
+		} catch (IOException | LZ78Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
